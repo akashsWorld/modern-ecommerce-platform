f1b13d4270ab4d3f4c9c147319ef6bf6034aa746
akashsWorld <akashbiswas.271999@gmail.com>
2024-02-26 06:00:48 +0530

    Project intialized. A react-app and a mvn project is created.

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello, <br />
+      Welcome
+      </h1>
+      <p>The project is under Development.....</p>
+    </div>
+  )
+}
+
+export default App
