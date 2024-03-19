dce116350e8993dc854432d8711d4b8719eaab7c
Akash Biswas <akashbiswas.271999@gmail.com>
2024-03-20 03:28:16 +0530

    Change the lable name to categories.

--- client/src/components/Categories/Categories.tsx
+++ client/src/components/Categories/Categories.tsx
@@ -40,7 +40,7 @@ const Categories = ({ categoryList , productProp }:ProductPropsWithCategoryList 
         }
         style={{ width: 500 }}
         renderInput={(params) => (
-          <TextField {...params} label="Fixed tag" placeholder="Favorites" />
+          <TextField {...params} label="Categories"/>
         )}
       />
     </React.Fragment>
